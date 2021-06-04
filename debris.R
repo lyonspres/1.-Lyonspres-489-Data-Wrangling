32bfc71072effc10a5090621869f6d06789ce798
Preston Lyons <prestonlyons15@gmail.com>
2021-06-04 18:34:25 +1200

    Sorting Tables

--- debris.R
+++ debris.R
@@ -56,3 +56,11 @@ psych::fa(df1[c(td_test_adult, td_mem_adult, td_sug_adult)], rotate = "oblimin",
 
 psych::fa(df1[c(td_test_adult, td_mem_adult, td_sug_adult, id_test_adult, id_mem_adult, id_sug_adult, td_test_6_11, td_mem_6_11, td_sug_6_11, td_test_3_5, td_mem_3_5, td_sug_3_5, id_test_child, id_mem_child, id_sug_child)], rotate = "oblimin", nfactors = 12) %>%
   print.psych(., cut = .40, sort = T)
+
+
+
+
+df1$dn_id <- rowMeans(df1[paste0(c(id_test_child, id_mem_child, id_sug_child), "_bin")])
+df1$dn_td <- rowMeans(df1[paste0(c(td_test_3_5, td_mem_3_5, td_sug_3_5, td_test_6_11, td_mem_6_11, td_sug_6_11), "_bin")])
+df1$dn_id_ad <- rowMeans(df1[paste0(c(id_test_adult, id_mem_adult, id_sug_adult), "_bin")])
+df1$dn_td_ad <- rowMeans(df1[paste0(c(td_test_adult, td_mem_adult, td_sug_adult), "_bin")])
